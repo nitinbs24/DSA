fa44063e6c895f60b69956e0a68bb043fcfee498
nitinbs24 <nitinskatte@gmail.com>
2026-07-25 11:30:40 +0530

    Binary search solutions for square root and nth root problems

---
+++ Binary Search/BS on Answers/01 Finding Square Root/solution.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int mySqrt(int x){
+    if(x<2) return x;
+
+    int low=1,high=x/2,ans;
+
+    while(low<=high){
+        int mid=low+(high-low)/2;
+
+        if(mid*mid<=x){ 
+            ans=mid;
+            low=mid+1;
+        }
+        else high=mid-1;
+    }
+    return ans;
+}
+
+int main(){
+    int x=16;
+    cout<<mySqrt(x)<<endl;
+}
